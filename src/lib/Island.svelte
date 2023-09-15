deada4986a13add95b20a7cca4fb728188da2a00
invakid404 <invakid404@riseup.net>
2023-09-15 21:27:28 +0300

    feat: implement `Island` component

--- src/lib/Island.svelte
+++ src/lib/Island.svelte
@@ -1,1 +1,44 @@
-<h1>Hello world</h1>
+<script lang="ts">
+	import * as devalue from 'devalue';
+	import type { ComponentType } from 'svelte';
+
+	/**
+	 * The component to hydrate
+	 */
+	export let component: ComponentType;
+
+	/**
+	 * The props to pass to the component
+	 */
+	export let props: Record<string, unknown> = {};
+
+	export let script: string | null = null;
+
+	export let islandId: string | null = null;
+</script>
+
+<svelte:head>
+	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+	{@html `
+		${'<scr'}ipt type="module" src="/__islands/is-land.js"></script>
+	`.trim()}
+</svelte:head>
+<is-land id="island-{islandId}" {...$$restProps}>
+	<svelte:component this="{component}" />
+	<template data-island>
+		<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+		{@html `
+			${'<scr'}ipt type="module">
+				import * as component from "${script}";
+
+				const Component = Object.values(component)[0];
+
+				new Component({
+					target: document.getElementById("island-${islandId}"),
+					props: ${devalue.uneval(props)},
+					hydrate: true,
+				});
+			</script>
+	`.trim()}
+	</template>
+</is-land>
