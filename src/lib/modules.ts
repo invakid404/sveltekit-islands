deadc8de69082fe3799c0d75fe9f97254a7ac6cc
invakid404 <invakid404@riseup.net>
2024-10-01 21:59:56 +0300

    fix: handle template strings
    Svelte 5 formats the prop as a template string like so:
    
    ![](https://img.qilin-qilin.ts.net/raw/2024-10-01_18-57-23_w0wps.webp)
    
    This resulted in island imports not being properly substituted in the
    prerendered HTML. This patch tweaks the regex to catch cases like this.

--- src/lib/modules.ts
+++ src/lib/modules.ts
@@ -1,5 +1,7 @@
 export const ISLAND_MODULE_PREFIX = 'island';
 
-export const ISLAND_MODULE_PATTERN = new RegExp(`"/${ISLAND_MODULE_PREFIX}:(\\S+?):"`, 'g');
-
+export const ISLAND_MODULE_PATTERN = new RegExp(
+	`(?:"|\`)/${ISLAND_MODULE_PREFIX}:(\\S+?):.*?(?:"|\`)`,
+	'g'
+);
 export const SVELTE_CHUNK = 'svеltе-сhunk';
